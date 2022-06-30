4e77f5f5cac9c028be4de08fdf50510fb9055795
Nathan Thorpe <nathan@techstormpc.com>
2022-06-29 22:27:54 -0700

    add missing dependencies

--- setup.py
+++ setup.py
@@ -14,7 +14,8 @@ entry_points = {
 install_requires = [
     "click>=8.0,<8.1",
     "questionary==1.10.0",
-    "requests==2.27.1"
+    "requests==2.27.1",
+    "azure-identity>=1.8.0"
 ]
 
 setup(
@@ -22,10 +23,22 @@ setup(
     version='0.0.1',
     author='TechStorm PC',
     license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        "Intended Audience :: Information Technology",
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
     author_email='nathan@techstormpc.com',
     description='SDK for interacting with the Microsoft Partner Center',
     packages=find_packages(include=['partner_tools*']),
     install_requires=install_requires,
     url='https://github.com/techstormpc/ms-partner-tools',
-    entry_points=entry_points
+    entry_points=entry_points,
+    long_description=long_description
 )
